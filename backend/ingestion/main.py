e77178f0efd68e13f9e5287e9efcf3a7b27176b7
David Tobasura <hollerller@gmail.com>
2026-05-05 00:02:15 -0500

    ingestion service reads uart and pusblishes to rabbit mq

---
+++ backend/ingestion/main.py
@@ -0,0 +1,16 @@
+import serial
+import pika
+
+ser = serial.Serial("/dev/tty.usbmodem11203")
+ser.baudrate = 115200
+print(ser.name)
+
+connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
+channel = connection.channel()
+channel.queue_declare(queue="morse")
+
+
+while True:
+    line = ser.readline()
+    channel.basic_publish(exchange="", routing_key="morse", body=line.decode().strip())
+    print(line.decode().strip())
